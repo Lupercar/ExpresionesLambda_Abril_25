664d2544b2083c46bf491c50246b24ac7606632e
JLGG <hotolete@gmail.com>
2016-05-02 19:17:52 +0200

    Creo nueva expresion lambda que aplica el valor absoluto, multiplica por 2 y acumula, mediante la clase Math.abs()

--- src/main/App.java
+++ src/main/App.java
@@ -9,14 +9,17 @@ public class App {
 
 		// le doy valores
 		cont.setNumero(0, 1.0);
-		cont.setNumero(1, 2.0);
-		cont.setNumero(3, 3.0);
+		cont.setNumero(1, -2.0);
+		cont.setNumero(2, 3.0);
 
-//		ver contenido de una array
+		// ver contenido de una array
 		System.out.println(cont);
-		
-//		hacemos la suma mediante la interfaz funcional tiene un parametro le
-//		llamo v al parametro -> cada elemento lo multiplico por 2 y lo sumo
-		System.out.println("La suma del array vale: "+cont.suma(v -> v*2));
+
+		// hacemos la suma mediante la interfaz funcional tiene un parametro le
+		// llamo v al parametro -> cada elemento lo multiplico por 2 y lo sumo
+		System.out.println("La suma del array vale: " + cont.suma(v -> v * 2));
+
+		// Multiplico por 2, le aplico el valor absoluto y sumo
+		System.out.println("La suma valor absoluto del array vale: " + cont.suma(v -> Math.abs(v * 2)));
 	}
 }
