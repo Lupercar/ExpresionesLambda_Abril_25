5ec067a8eaca2722f9670aa5b20aaf2df1ac2133
JLGG <hotolete@gmail.com>
2016-05-02 17:13:11 +0200

    Creo una clase ContenedorNumeros que tiene como propiedad de array de numeros tipo double

---
+++ src/main/ContenedorNumeros.java
@@ -0,0 +1,39 @@
+package main;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
+public class ContenedorNumeros {
+	// Hacemos que almacene un Array de Numeros
+	private double[] numeros;
+
+	public ContenedorNumeros(int numeroElementos) {
+
+		if (numeroElementos > 0)
+			// constructor que crea el array en memoria
+			numeros = new double[numeroElementos];
+	}
+
+	// metodo que nos devuelve el contenido de un numero en una cierta posicion
+	public double getNumero(int posicion) {
+		double retorno = 0.0;
+		if ((posicion >= 0) && posicion < numeros.length)
+			retorno = numeros[posicion];
+		else
+			throw new ArrayIndexOutOfBoundsException("Posicion fuera del rango de los valores");
+		return retorno;
+	}
+
+	public void setNumero(int posicion, double valor) {
+		// la posicion sobreescribe si ya existe
+		if ((posicion >= 0) && posicion < numeros.length) {
+			numeros[posicion] = valor;
+		}
+		else 
+			throw new ArrayIndexOutOfBoundsException("Posicion fuera del rango de los valores"); 
+	}
+	
+	public double suma(ITransformacion transformacion){
+		//A cada elemento le aplica una cierta transformacion y luego acumula el valor
+		
+	}
+}
