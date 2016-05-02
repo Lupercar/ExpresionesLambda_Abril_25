4adb5864d061e61faaaef729aac0d46f2349f8ae
JLGG <hotolete@gmail.com>
2016-05-02 18:55:40 +0200

    Le paso al metodo transformacion la expresion lambda correspondiente en este caso, cada elemento del array sera multiplicado por 2 y acumulado.

--- src/main/App.java
+++ src/main/App.java
@@ -3,16 +3,17 @@ package main;
 public class App {
 
 	public static void main(String[] args) {
-		
-//		creo el objeto de tipo ContenedorNumeros
-		ContenedorNumeros cont = new ContenedorNumeros(6); 
-		
-//		le doy valores 
+
+		// creo el objeto de tipo ContenedorNumeros
+		ContenedorNumeros cont = new ContenedorNumeros(6);
+
+		// le doy valores
 		cont.setNumero(0, 1.0);
 		cont.setNumero(1, 2.0);
 		cont.setNumero(3, 3.0);
-		
-//		hacemos la suma
-		cont.suma(transformacion); 
+
+//		hacemos la suma mediante la interfaz funcional tiene un parametro le
+//		llamo v al parametro -> cada elemento lo multiplico por 2 y lo sumo
+		System.out.println(cont.suma(v -> v*2));
 	}
 }
